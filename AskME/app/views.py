699c33568afc3464b01bc342ea0a123d0cdafe15
Vsevolod Donchenko <vsevolond@gmail.com>
2022-11-09 23:38:14 +0300

    [ADD] Connected django to templates

--- AskME/app/views.py
+++ AskME/app/views.py
@@ -1,3 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'auth_ok/index.html')
+
+def ask(request):
+    return render(request, 'auth_ok/ask.html')
+
+def settings(request):
+    return render(request, 'auth_ok/settings.html')
+
+# def question(request, id: int):
+#     return render(request, 'auth_ok/question.html')
+def question(request):
+    return render(request, 'auth_ok/question.html')
+
+def tag(request):
+    return render(request, 'auth_ok/tag.html')
+
+def login(request):
+    return render(request, 'not_auth/login.html')
+
+def signup(request):
+    return render(request, 'not_auth/signup.html')

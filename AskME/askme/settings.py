2ed7e18fea397b2173925bb0cd3bd46abeaf2b39
Vsevolod Donchenko <vsevolond@gmail.com>
2022-11-14 23:37:37 +0300

    [ADD] Connected to postgres and added date for answers

--- AskME/askme/settings.py
+++ AskME/askme/settings.py
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'askme.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'askme',
+       'USER': 'vsevolod',
+       'PASSWORD': '',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
+
 
 
 # Password validation

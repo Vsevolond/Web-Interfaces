9b5c2309d24777c7a986f385d2afad25e41bd112
Vsevolod Donchenko <vsevolond@gmail.com>
2022-11-15 19:48:01 +0300

    [ADD] Added models

--- AskME/app/models.py
+++ AskME/app/models.py
@@ -1,3 +1,4 @@
+import django.contrib.auth.backends
 from django.db import models
 
 IS_AUTH = True
@@ -40,3 +41,35 @@ for id in range(100):
         'tags': [f'tag{i}' for i in range(id)]
     })
 
+
+class Member(models.Model):
+    info = models.ForeignKey(django.contrib.auth.backends.UserModel, on_delete=models.CASCADE)
+    avatar = models.ImageField()
+    rank = models.IntegerField()
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    date = models.DateField(auto_now=True)
+    author = models.ForeignKey(Member, on_delete=models.SET_NULL, null=True, related_name="questions")
+    like_users = models.ManyToManyField(Member, related_name="likes")
+    dislike_users = models.ManyToManyField(Member, related_name="dislikes")
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    date = models.DateField(auto_now=True)
+    correct = models.BooleanField(default=False)
+    author = models.ForeignKey(Member, on_delete=models.SET_NULL, null=True, related_name="answers")
+    like_users = models.ManyToManyField(Member, related_name="likes")
+    dislike_users = models.ManyToManyField(Member, related_name="dislikes")
+    question = models.ForeignKey(Question, on_delete=models.CASCADE,
+                                 related_name="answers", related_query_name="answer")
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=10)
+    rank = models.IntegerField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE,
+                                 related_name="tags", related_query_name="tag")

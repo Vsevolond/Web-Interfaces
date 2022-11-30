0320db9ec92a85cda04d68973e46d0308e9251d6
Vsevolod Donchenko <vsevolond@gmail.com>
2022-11-30 20:59:16 +0300

    Update models.py

--- AskME/app/models.py
+++ AskME/app/models.py
@@ -7,44 +7,6 @@ from django.contrib.contenttypes.models import ContentType
 
 IS_AUTH = True
 
-HOT_QUESTIONS = [
-    1, 4, 7
-]
-
-POP_TAGS = [
-    {'name': 'Swift', 'color': "btn btn-danger"},
-    {'name': 'Xcode', 'color': "btn btn-warning"},
-    {'name': 'tag7', 'color': "btn btn-success"},
-    {'name': 'html', 'color': "btn btn-primary"},
-    {'name': 'css', 'color': "btn btn-secondary"},
-    {'name': 'tag4', 'color': "btn btn-dark"},
-]
-
-BEST_MEMBERS = [
-    'Mr. Freeman',
-    'Dr. House',
-    'Bender',
-    'Queen Victoria',
-    'V. Pupkin'
-]
-
-QUESTIONS = []
-for id in range(100):
-    ANSWERS = []
-    for i in range(id * id):
-        ANSWERS.append({
-            'id': i,
-            'text': f'Text of Answer № {i}'
-        })
-    QUESTIONS.append({
-        'id': id,
-        'title': f'Question № {id}',
-        'text': f'Text of Question № {id}',
-        'ans_num': id * id,
-        'answers': ANSWERS,
-        'tags': [f'tag{i}' for i in range(id)]
-    })
-
 
 class MemberManager(models.Manager):
     def best_members(self):
